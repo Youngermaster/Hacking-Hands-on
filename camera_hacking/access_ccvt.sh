64defdb80a4e8422a03b623b94b0ed2d984eecb3
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-12-03 12:46:45 -0500

    🐍 Little change on a Python dependency

--- camera_hacking/access_ccvt.sh
+++ camera_hacking/access_ccvt.sh
@@ -1,5 +1,5 @@
 # If you are using Windows just go to the respectives pages to download the tool
-sudo apt-get install git python3 -y
+sudo apt-get install git python3 python3-pip -y
 pip3 install requests
 pip3 install colorama
 git clone https://github.com/AngelSecurityTeam/Cam-Hackers
